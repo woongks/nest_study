efd0daecdbd8b417a97286f34270bca8ac92490c
ksw <tlsdnddl12@naver.com>
2022-06-13 22:46:40 +0900

    added create user service,controller, log

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
-
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password }); //user 엔티티 인스턴스를 생성
+    return this.repo.save(user); // 해당 인스턴스를 db에 저장
+  }
+}
