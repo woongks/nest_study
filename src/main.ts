0fc293f9277ffc46ae5dcc75c7401b21e406feec
ksw <tlsdnddl12@naver.com>
2022-06-13 18:59:38 +0900

    created user dto and set validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //만약 형식에 맞지 않는 data를 받아오면 해당 항목만 무시한다.
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
